0280f5a475021be5309731da481410fa8d21ca11
Jagannath Kudale <jagannath.kudale72@gmail.com>
2026-07-24 19:07:22 +0530

    SSH Test

--- tests/01-locators/01-getByRole.spec.ts
+++ tests/01-locators/01-getByRole.spec.ts
@@ -3,7 +3,7 @@ import { test } from '@playwright/test';
 test('getByRole Example', async ({ page }) => {
 
     await page.goto('https://www.google.com');
-
+  await page.waitForTimeout(3000);
     await page.getByRole('combobox').fill('Playwright');
     await page.waitForTimeout(3000);
 
